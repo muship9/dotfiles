23679376e76ba007c8d4101d6f73091ecd630c8a
muship9 <shinp09@gmail.com>
2024-08-12 20:27:12 +0900

    リンク先を .config に変更

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ dotfiles=(~/dotfiles/.zshrc ~/dotfiles/starship/starship.toml ~/dotfiles/wezterm
 
 # .zshrc のシンボリックリンクをホームディレクトリ直下に作成する
 for file in "${dotfiles[@]}"; do
-  ln -svf $file ~/
+  ln -svf $file ~/.config
 done
