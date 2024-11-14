6ebedee1df2008542c21499ec41ab768077fd6a4
muship9 <57585657+shinp09@users.noreply.github.com>
2024-11-14 21:55:08 +0900

    fix: 隠しファイルを表示されるように修正

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+local g = vim.g
 
-opt.relativenumber = false
+opt.relativenumber = false -- 絶対行番号を使用
+opt.hidden = true -- 非表示のバッファに切り替えを許可
+
+g.nvim_tree_hide_dotfiles = 0 -- nvim-treeでドットファイルを表示
 
