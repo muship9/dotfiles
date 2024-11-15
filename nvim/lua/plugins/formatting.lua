382f7729342c190bac1d2473062cb4775ce91b32
muship9 <shinp09@gmail.com>
2024-11-15 18:34:53 +0900

    fix: vim のエラー解消

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -1,23 +1,20 @@
--- ~/.config/nvim/lua/plugins/formatting.lua
+-- 保存時に自動フォーマットを実行するためのautocmd
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.json" },
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
+
 return {
-    -- null-lsを使ってPrettierを設定
-    {
-        "jose-elias-alvarez/null-ls.nvim",
-        opts = function(_, opts)
-            local null_ls = require("null-ls")
-            -- Prettierを追加
-            table.insert(opts.sources, null_ls.builtins.formatting.prettier)
-        end,
-    },
-
-    -- 保存時に自動フォーマットを実行
-    {
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.json" },
-            callback = function()
-                vim.lsp.buf.format()
-            end,
-        }),
-    },
+  -- null-lsを使ってPrettierを設定
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local null_ls = require("null-ls")
+      opts.sources = opts.sources or {} -- opts.sourcesがnilの場合に空のテーブルとして初期化
+      -- Prettierを追加
+      table.insert(opts.sources, null_ls.builtins.formatting.prettier)
+    end,
+  },
 }
-
