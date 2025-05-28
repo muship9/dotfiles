0baa67111d76eacba37b8bd1dc6c38d439186e8d
muship9 <shinp09@gmail.com>
2025-05-28 13:24:48 +0900

    cp de

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- 現在のファイルの相対パス（カレントディレクトリから）をクリップボードにコピー
+vim.keymap.set("n", "<leader>cp", function()
+  local relative_path = vim.fn.fnamemodify(vim.fn.expand("%"), ":.")
+  vim.fn.setreg("+", relative_path)
+  print("Copied to clipboard: " .. relative_path)
+end, { desc = "Copy relative path to clipboard" })
