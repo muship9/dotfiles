fea734f6a69f12385a120bdd342cc46c3076c097
muship9 <shinp09@gmail.com>
2025-08-04 14:39:12 +0900

    現在のディレクトリ名をタブに表示させた

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,19 @@
 local wezterm = require 'wezterm';
 
+-- タブタイトルを現在のディレクトリ名に設定する関数
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+  local title = tab.active_pane.title
+  -- プロセス名とパスから現在のディレクトリを取得
+  if tab.active_pane.current_working_dir then
+    local cwd = tab.active_pane.current_working_dir.file_path
+    local basename = string.match(cwd, "([^/]+)$")
+    if basename then
+      title = basename
+    end
+  end
+  return title
+end)
+
 return {
   -- font
   font = wezterm.font('JetBrains Mono', { weight = 'Medium', italic = true }),
@@ -11,6 +25,9 @@ return {
   color_scheme = "Kanagawa (Gogh)",
 
   tab_bar_at_bottom = true,
+
+  -- タブタイトルの設定
+  tab_max_width = 32,
 
   automatically_reload_config = true,
 
