3a7fb2fb133cc96b8960d72aec3cde56441c35ef
ShinpeiMukaiyama <shinpeimukaiyama@ShinpeiMukaiyamanoMacBook-Pro.local>
2025-11-11 10:30:35 +0900

    コメントの色を指定

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -1,7 +1,32 @@
+-- TODO : ほげ
+-- NOTE : ほげ
+-- WARN : ほげ
 return {
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {},
-	},
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      keywords = {
+        TODO = {
+          icon = " ",
+          color = "todo",
+        },
+        NOTE = {
+          icon = " ",
+          color = "note",
+          alt = { "INFO" },
+        },
+        WARN = {
+          icon = " ",
+          color = "warning",
+          alt = { "WARNING", "XXX" },
+        },
+      },
+      colors = {
+        todo = { "#9ccfd8" },    -- rose-pine foam (シアン)
+        note = { "#908caa" },    -- rose-pine subtle (グレー)
+        warning = { "#f6c177" }, -- rose-pine gold (オレンジ/黄色)
+      },
+    },
+  },
 }
